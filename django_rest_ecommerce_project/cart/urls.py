986181f0a1ca8cd91aabcdb7c97bb7b12a20627a
Mehdi <mmmehdiseyfi@gmail.com>
2026-01-03 14:17:25 +0330

    add cart-totals/ with CartTotalsApi.as_view()

--- django_rest_ecommerce_project/cart/urls.py
+++ django_rest_ecommerce_project/cart/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
-from django_rest_ecommerce_project.cart.apis import CartApi, CartItemApi, CartItemDetailApi, CartClearApi
+from django_rest_ecommerce_project.cart.apis import CartApi, CartItemApi, CartItemDetailApi, CartClearApi, CartTotalsApi
 
 urlpatterns = [
     path("items/", CartItemApi.as_view(), name="add-item-to-cart" ),
     path("items/<int:item_id>/", CartItemDetailApi.as_view(), name="cart-item-detail"),
     path("clear-cart/", CartClearApi.as_view(), name="clear-cart"),
+    path("cart-totals/", CartTotalsApi.as_view(), name="cart_totals"),
     
     path("", CartApi.as_view(), name="cart-detail"),
     path("<slug:slug>/", CartApi.as_view(), name="cart-by-slug"),
