ac6ceb98befaa4b74268c05cb2e9a4250b060a0b
Mehdi <mmmehdiseyfi@gmail.com>
2026-01-03 12:50:11 +0330

    add clear-cart/ with ClearCartApi.as_view()

--- django_rest_ecommerce_project/cart/urls.py
+++ django_rest_ecommerce_project/cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from django_rest_ecommerce_project.cart.apis import CartApi, CartItemApi, CartItemDetailApi
+from django_rest_ecommerce_project.cart.apis import CartApi, CartItemApi, CartItemDetailApi, CartClearApi
 
 urlpatterns = [
     path("items/", CartItemApi.as_view(), name="add-item-to-cart" ),
     path("items/<int:item_id>/", CartItemDetailApi.as_view(), name="cart-item-detail"),
+    path("clear-cart/", CartClearApi.as_view(), name="clear-cart"),
     
     path("", CartApi.as_view(), name="cart-detail"),
     path("<slug:slug>/", CartApi.as_view(), name="cart-by-slug"),
